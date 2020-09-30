36418882eda37f60d9ebf208fd4f116328d702e5
devho813 <devho813@gmail.com>
2020-09-30 21:50:32 +0900

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,8 @@
+import React from 'react';
 import styled from 'styled-components';
 
 export default function Home() {
-  return (
-    <HomeContainer>
-      <h1>👋 Chanho`s Boilerplate</h1>
-      <Chanho
-        src="https://avatars2.githubusercontent.com/u/29947014?s=460&u=4749fe67e031e967bb5659895d0dc8e1f9365205&v=4"
-        alt="chanho image"
-      />
-    </HomeContainer>
-  );
+  return <HomeContainer></HomeContainer>;
 }
 
 const HomeContainer = styled.div`
@@ -20,11 +13,4 @@ const HomeContainer = styled.div`
     color: #f1c40f;
     font-size: 25px;
   }
-`;
-
-const Chanho = styled.img`
-  display: inline-block;
-  margin-top: 10px;
-  width: 100px;
-  border-radius: 5px;
 `;
