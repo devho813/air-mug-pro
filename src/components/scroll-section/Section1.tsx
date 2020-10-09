240d660acef34f3de69784a4aeca31f824fc312c
devho813 <devho813@gmail.com>
2020-10-10 04:26:01 +0900

    section1, 2 태그 생성

--- src/components/scroll-section/Section1.tsx
+++ src/components/scroll-section/Section1.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
 function Section1() {
-  return <section></section>;
+  return (
+    <section>
+      <p>
+        <strong>보통 스크롤 영역</strong>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem suscipit voluptatum veritatis itaque iure
+        sapiente nulla dicta qui corporis quaerat vitae ducimus, beatae fuga est cupiditate quidem error nostrum minus
+        libero quod impedit laudantium! Tempora delectus rerum ullam provident ea, facilis sapiente excepturi iste,
+        explicabo neque quam deserunt commodi illum repellendus exercitationem aliquid dolorum molestias accusantium
+        aliquam cum praesentium assumenda obcaecati. Fugiat optio rem, maiores quibusdam autem incidunt aliquid placeat
+        quae accusantium veritatis ab repudiandae voluptatibus eius nihil quia fugit laudantium. Fuga alias voluptate,
+        laborum nobis ex ut rem provident excepturi saepe repudiandae cumque corporis sequi laudantium voluptatem animi,
+        officiis qui. Molestiae inventore a in tempore dignissimos quam cumque, possimus consectetur corporis facere
+        ducimus ipsa, quisquam necessitatibus cum sequi. Optio, ad vitae non reprehenderit modi repudiandae possimus
+        ipsum sint dicta expedita eveniet soluta quod! Qui vitae ea reprehenderit ducimus iste ipsum autem, alias quae
+        facilis, rerum, quas nobis et. Dignissimos eum animi similique alias, eaque blanditiis molestias dolor quos
+        porro quam dicta temporibus! Exercitationem praesentium quo voluptate dignissimos magnam! Nostrum aut incidunt
+        quidem dolores delectus repudiandae consectetur mollitia, quia obcaecati distinctio voluptates blanditiis
+        possimus magnam ratione odio corrupti excepturi a sint sapiente odit ad temporibus voluptatibus unde? Ullam, hic
+        ratione!
+      </p>
+    </section>
+  );
 }
 export default Section1;
